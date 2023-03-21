fa46df5fe9fd805d5a9f905618bed598af9df08e
Jakob F Gregersen <lilleper.jfg@gmail.com>
2023-03-21 20:19:20 +0100

    feat: implement dijkstra

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ Install requirements with ```pip3 install -r requirements.txt```
 ## linter
 Run linter with command: ```pylint $(git ls-files '*.py')```
 
+## Dijkstra
+Maintain a table of three columns, with a row for each node.
+The columns are "Node", "Shortest distance from eg. A", "Previous Node"
+
+1. Create two lists, one of visited nodes and one of unvisited nodes
+2. Select a starting node
+3. Note down all distances as infinity
+4. Update the connecting vertices weights and previous node
+5. Remove starting node from list of unvisited.
+6. Select node from unvisited nodes-list with smallest weight in table, repeat 4. and 5.
--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -1,12 +1,49 @@
 '''Module that contains the dijkstra's shortest path implementation'''
 import json
 
+def dijkstra(graph : 'dict[str, dict[str,int]]',
+             source: str) -> 'dict[str, tuple[int, str]]':
+    '''Receives a graph and a starting node, returns a table of shortest paths and previous nodes'''
+    unvisited_nodes: list[str] = []
+    shortest_path: dict[str, tuple[int, str]] = {}
+    for key in graph:
+        unvisited_nodes.append(key)
+        if key != source:
+            shortest_path[key] = (1000, "null")
+        else:
+            shortest_path[key] = (0, "source")
+    while len(unvisited_nodes) != 0:
+        selected_node = find_smallest_distance(shortest_path, unvisited_nodes)
+        unvisited_nodes.remove(selected_node)
+        shortest_path = update_shortest_path(selected_node,
+                                             graph[selected_node],
+                                             shortest_path)
+    return shortest_path
+
+def find_smallest_distance(shortest_path: 'dict[str, tuple[int, str]]',
+                           unvisited_nodes: 'list[str]' )-> str:
+    '''Finds the unvisited node with the least distance from source node'''
+    minimum: int = 1000
+    smallest: str = "null"
+    for elem in unvisited_nodes:
+        if shortest_path[elem][0] < minimum:
+            smallest: str = elem
+    return smallest
+
+def update_shortest_path(source: str,
+                    neighbors: 'dict[str,int]',
+                    shortest_path: 'dict[str, tuple[int, str]]') -> 'dict[str, tuple[int, str]]':
+    '''Updates shortest path thrugh some given source node'''
+    for key, value in neighbors.items():
+        if value + shortest_path[source][0] < shortest_path[key][0]:
+            shortest_path[key] = value + shortest_path[source][0], source
+    return shortest_path
+
 def main():
     '''Function main, runs the module'''
-    # Read in the graph
     with open("./graphs/A-E.json", "r", encoding="utf-8") as read_file:
-        data = json.load(read_file)
-    print(data)
+        data: dict[str,dict[str,int]] = json.load(read_file)
+    print(dijkstra(data, "A"))
 
 if __name__ != "main":
     main()
